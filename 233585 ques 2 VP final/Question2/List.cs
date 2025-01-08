fba31575667b2a4bd39b8bedbc38a5a85f9a27c8
Shah-Abdullah09 <233585@students.au.edu.pk>
2025-01-08 15:57:09 +0500

    Add files via upload

---
+++ 233585 ques 2 VP final/Question2/List.cs
@@ -0,0 +1,16 @@
+﻿using Question2.Components.Pages;
+
+public static class ProductDataService
+{
+    public static List<Product> products = new List<Product>();
+
+    public static void AddProduct(Product p)
+    {
+        products.Add(p);
+    }
+
+    public static List<Product> GetProducts()
+    {
+        return products;
+    }
+}
---
+++ 233585 ques 2 VP final/Question2/Product.cs
@@ -0,0 +1,13 @@
+﻿// File: Models/Product.cs
+public class Product
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public string Category { get; set; }
+    public Product()
+    {
+        Name = "";
+        Price = 0;
+        Category = "";
+    }
+}
